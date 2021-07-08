fb21208396bf04a98e72087b42da0cb881b4e334
jkubala <33606724+jkubala@users.noreply.github.com>
2021-07-08 15:46:26 +0200

    Game loop finished

--- src/com/te/hangman/GameManager.java
+++ src/com/te/hangman/GameManager.java
@@ -8,11 +8,15 @@ import java.util.Scanner;
 
 public class GameManager {
 	Scanner scanner = new Scanner(System.in);
-	int nOfTriesLeft = 15;
+	int defaultNOfTriesLeft = 15;
+	int nOfTriesLeft = defaultNOfTriesLeft;
+	String wordToGuess = "";
+	String wordToGuessUnderscore = "";
+	boolean gameWon = false;
 	boolean exitGame = false;
 
 	enum GameState {
-		MAIN_MENU, GAME_LOOP
+		MAIN_MENU, GAME_LOOP, END_BOARD
 	}
 
 	GameState gameState = GameState.MAIN_MENU;
@@ -27,11 +31,43 @@ public class GameManager {
 			}
 			case GAME_LOOP: {
 				handleGameLogic();
+				break;
+			}
+			case END_BOARD: {
+				displayEndBoard();
+				break;
 			}
 			default:
 				break;
 			}
 		} while (!exitGame);
+	}
+
+	private void displayEndBoard() {
+		System.out.println("1. Play again\n2. Exit");
+		int userinput = 0;
+		try {
+			userinput = scanner.nextInt();
+			scanner.nextLine();
+		} catch (InputMismatchException e) {
+			scanner.nextLine();
+		}
+
+		switch (userinput) {
+		case 1: {
+			resetGameVariables();
+			gameState = GameState.GAME_LOOP;
+			break;
+		}
+		case 2: {
+			exitGame = true;
+			break;
+		}
+		default:
+			System.out.println("Invalid choice");
+			scanner.nextLine();
+			break;
+		}
 	}
 
 	private void handleMainMenu() {
@@ -71,20 +107,81 @@ public class GameManager {
 		default:
 			System.out.println("Invalid choice");
 			scanner.nextLine();
+			break;
 		}
 	}
-	
 
 	private void handleGameLogic() {
-		System.out.println("Playing game");
+		if (wordToGuess.trim().isEmpty()) {
+			setWordToGuess();
+			wordToGuess = wordToGuess.toUpperCase();
+			setDisplayedWordToGuess();
+		}
+		printGameScreen();
+		handleGuessing();
+	}
+
+	private void setDisplayedWordToGuess() {
+		for (int i = 0; i < wordToGuess.length(); i++) {
+			wordToGuessUnderscore += "_ ";
+		}
+	}
+
+	private void handleGuessing() {
+		System.out.println("Enter a letter, or whole word");
+		String guess = scanner.nextLine();
+		guess = guess.toUpperCase();
+		if (guess.length() == 0) {
+			System.out.println("Error - empty guess inputed!");
+		} else if (guess.length() == 1) {
+			if (wordToGuess.indexOf(guess.charAt(0)) != -1) {
+				updateDisplayedSecretWord(guess.charAt(0));
+				if (wordToGuessUnderscore.indexOf('_') == -1) {
+					gameState = GameState.END_BOARD;
+				}
+			} else {
+				nOfTriesLeft--;
+			}
+		} else {
+			if (wordToGuess.equals(guess)) {
+				gameState = GameState.END_BOARD;
+			} else {
+				nOfTriesLeft--;
+			}
+		}
+	}
+
+	private void setWordToGuess() {
+		boolean validGuessWord = false;
+		do {
+			clearScreen();
+			System.out.println("Enter word, that will be guessed:");
+			wordToGuess = scanner.nextLine();
+			if (!wordToGuess.trim().isEmpty() && wordToGuess.length() > 1 && wordToGuess.matches("[a-zA-Z]+")) {
+				validGuessWord = true;
+			} else {
+				System.out.println("Invalid word entered!");
+				scanner.nextLine();
+			}
+		} while (!validGuessWord);
+		clearScreen();
+		System.out.println("You chose:\n" + wordToGuess + "\nEnter \"Y\" to change it, or press Enter to continue");
+		if (scanner.nextLine().toLowerCase().equals("y")) {
+			wordToGuess = "";
+			setWordToGuess();
+		}
+	}
+
+	private void printGameScreen() {
+		clearScreen();
+		System.out.println("Tries left: " + nOfTriesLeft + "\nSecret word:\n" + wordToGuessUnderscore);
 	}
 
 	private void showCurrentPlayers() {
 		clearScreen();
 		if (players.size() > 0) {
 			System.out.println("Current players:\n" + Arrays.toString(players.toArray()));
-		}
-		else {
+		} else {
 			System.out.println("There have not been any players created yet");
 		}
 		scanner.nextLine();
@@ -123,14 +220,27 @@ public class GameManager {
 	}
 
 	private void deletePlayer() {
+		if (players.size() == 0) {
+			System.out.println("There are no player that could be deleted!");
+			scanner.nextLine();
+			return;
+		}
 		String playerToDelete;
 		System.out.println("Enter player name:");
 		playerToDelete = scanner.nextLine();
 		if (playerExists(playerToDelete)) {
-			System.out.println("WEEEE");
 			deletePlayerByID(playerToDelete);
 		}
+	}
 
+	private void updateDisplayedSecretWord(char letterToUpdateWith) {
+		char[] DispSecWordArr = wordToGuessUnderscore.toCharArray();
+		for (int i = 0; i < wordToGuess.length(); i++) {
+			if (wordToGuess.charAt(i) == letterToUpdateWith) {
+				DispSecWordArr[i * 2] = letterToUpdateWith;
+			}
+		}
+		wordToGuessUnderscore = String.valueOf(DispSecWordArr);
 	}
 
 	private void clearScreen() {
@@ -144,5 +254,13 @@ public class GameManager {
 
 	private void deletePlayerByID(final String name) {
 		players.removeIf(player -> player.getName().equals(name));
+	}
+	
+	private void resetGameVariables()
+	{
+		gameWon = false;
+		nOfTriesLeft = defaultNOfTriesLeft;
+		wordToGuess = "";
+		wordToGuessUnderscore = "";
 	}
 }
